0f6827824adb2bd052bd5b046b16c933b4474f5e
Bader1011 <ibader1011@gmail.com>
2019-02-09 03:45:36 +0300

    add lose and win alerts and console log for each attempt

--- main.js
+++ main.js
@@ -4,13 +4,17 @@ var wordList = ["desk", "books", "lab", "board"];
 var newArr = [];
 var textBox = document.getElementById("textBox");
 var index = [];
-
+var win = 0;
 var result = document.getElementById("result");
 var message = document.getElementById("gussingLetter1");
 
 var randomWords = wordList[Math.floor(Math.random() * wordList.length)];
 console.log(" the random word is", randomWords);
 randomWords = randomWords.split("");
+
+document.getElementById("TrayOtherWord").addEventListener("click", function() {
+  location.reload();
+});
 
 for (let i = 0; i < randomWords.length; i++) {
   var li = document.createElement("li");
@@ -25,26 +29,39 @@ for (let i = 0; i < randomWords.length; i++) {
 var gussingLetter1 = "";
 
 function printLetter() {
-  message.value = "";
+  // message.value = '';
   var inputTextBox = document.getElementById("inputTextBox").value;
 
-  console.log("user input", inputTextBox);
+  
 
   for (var i = 0; i < randomWords.length; i++) {
     if (inputTextBox == randomWords[i]) {
       index.push(i);
+      console.log("correct letter", inputTextBox);
     }
   }
-
+  console.log(index.length);
+  if (index.length <= 0) {
+    console.log("wrong letter", inputTextBox);
+    alert("wrong letter! please try again ");
+  }
   for (var i = 0; i < index.length; i++) {
     if (index.length > 0) {
+      win += 1;
       newArr.push(randomWords[index[i]]);
       var guessing = document.getElementById(`guessing${index[i]}`);
       guessing.style.backgroundColor = "green";
       guessing.style.color = "black";
       guessing.innerHTML = newArr[i];
+      console.log(newArr);
+      console.log(randomWords.length);
+      console.log(win);
+      if (win == randomWords.length) {
+        alert("You win, good job!");
+      }
     } else {
-      console.log("wrong letter");
+      // console.log("error")
+      // alert("Error Character")
       message.value = `${inputTextBox} wrong letter`;
     }
   }
@@ -53,5 +70,3 @@ function printLetter() {
   newArr = [];
   document.getElementById("inputTextBox").value = "";
 }
-
-
